2d052b49151ea4fb8e0a422d8d743f49de593a04
Markus Juenemann <markus@juenemann.net>
2017-05-30 10:22:59 +1000

    Add support for Passlib versions prior to 1.7

--- filter_plugins/filterciscohash.py
+++ filter_plugins/filterciscohash.py
@@ -2,6 +2,12 @@
 
 import passlib.hash
 
+# Version 1.7.0 introduced `passlib.hash.md5_crypt.using(salt_size=...)`.
+try:
+    md5_crypt = passlib.hash.md5_crypt.using
+except AttributeError:
+    md5_crypt = passlib.hash.md5_crypt
+
 class FilterModule(object):
     def filters(self):
         return {
@@ -13,18 +19,18 @@ class FilterModule(object):
 
     def ciscohash5(self, password):
         """https://passlib.readthedocs.io/en/stable/lib/passlib.hash.md5_crypt.html"""
-        return passlib.hash.md5_crypt.using(salt_size=4).hash(password)
+        return md5_crypt(salt_size=4).hash(password)
 
     def ciscohash7(self, password):
         """https://passlib.readthedocs.io/en/stable/lib/passlib.hash.cisco_type7.html"""
-        return str(passlib.hash.cisco_type7.hash(password))
+        return passlib.hash.cisco_type7.hash(password)
 
     def ciscohashpix(self, password, user=''):
         """https://passlib.readthedocs.io/en/stable/lib/passlib.hash.cisco_pix.html"""
-        return str(passlib.hash.cisco_pix.hash(password, user))
+        return passlib.hash.cisco_pix.hash(password, user)
 
     def ciscohashasa(self, password, user=''):
         """https://passlib.readthedocs.io/en/stable/lib/passlib.hash.cisco_asa.html"""
-        return str(passlib.hash.cisco_asa.hash(password, user))
+        return passlib.hash.cisco_asa.hash(password, user)
 
 
